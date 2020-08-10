4d7572eab40f2bda9d05a0f069c8f375a5680270
Mike Kletz <michael.kletz.27@gmail.com>
2020-08-09 21:49:04 -0500

    init

---
+++ functions.js
@@ -0,0 +1,42 @@
+
+function determineOrdinal(Day_Count) {
+  var Last_Digit = Day_Count.toString().slice(-1)
+  var ordinal = ''
+
+  switch(Last_Digit) {
+     case "1":
+       ordinal = 'st'
+       break;
+     case "2":
+       ordinal = 'nd'
+       break;
+     case "3":
+       ordinal = 'rd'
+       break;
+     default:
+       ordinal = 'th'
+       break;    
+  }
+  
+  return ordinal
+}
+
+function updateClock() {
+    var today = new Date();
+    var covidStart = new Date("03/14/2020");
+    var Difference_In_Time = covidStart.getTime() - today.getTime();
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    var Rounded_Days = Math.round(Difference_In_Days) * -1
+    var Ordinal = determineOrdinal(Rounded_Days)
+    var DayText = Rounded_Days + Ordinal
+
+    document.getElementById('d').innerHTML = DayText;
+    document.getElementById('h').innerHTML = today.getHours();
+    document.getElementById('m').innerHTML = today.getMinutes();
+    document.getElementById('s').innerHTML = today.getSeconds();
+}
+
+function clockGoBrrr() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
