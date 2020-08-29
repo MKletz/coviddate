6dcaf3188efc3b626cc35ea84952640305905a4a
Mike Kletz <michael.kletz.27@gmail.com>
2020-08-29 15:50:11 -0500

    removed semis

--- src/functions.js
+++ src/functions.js
@@ -5,37 +5,37 @@ function determineOrdinal(Day_Count) {
   switch(Last_Digit) {
      case "1":
        ordinal = 'st'
-       break;
+       break
      case "2":
        ordinal = 'nd'
-       break;
+       break
      case "3":
        ordinal = 'rd'
-       break;
+       break
      default:
        ordinal = 'th'
-       break;    
+       break    
   }
   
   return ordinal
 }
 
 function updateClock() {
-    var today = new Date();
-    var covidStart = new Date("03/01/2020");
-    var Difference_In_Time = covidStart.getTime() - today.getTime();
-    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    var today = new Date()
+    var covidStart = new Date("03/01/2020")
+    var Difference_In_Time = covidStart.getTime() - today.getTime()
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)
     var Rounded_Days = Math.round(Difference_In_Days) * -1
     var Ordinal = determineOrdinal(Rounded_Days)
     var DayText = Rounded_Days + Ordinal
 
-    document.getElementById('d').innerHTML = DayText;
-    document.getElementById('h').innerHTML = today.getHours();
-    document.getElementById('m').innerHTML = today.getMinutes();
-    document.getElementById('s').innerHTML = today.getSeconds();
+    document.getElementById('d').innerHTML = DayText
+    document.getElementById('h').innerHTML = today.getHours()
+    document.getElementById('m').innerHTML = today.getMinutes()
+    document.getElementById('s').innerHTML = today.getSeconds()
 }
 
 function clockGoBrrr() {
-  updateClock();
-  window.setInterval("updateClock()", 1);
+  updateClock()
+  window.setInterval("updateClock()", 1)
 }
